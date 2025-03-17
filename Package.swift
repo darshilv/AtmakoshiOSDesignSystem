5376f4ecdf9cb31bfdfff9d5b5b405d147e157b1
Ashna Kothari <ashnakothari@gmail.com>
2025-03-17 10:00:53 -0700

    Added remote repo for dependency mgmt

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
             name: "AtmakoshDesignSystem",
             targets: ["AtmakoshDesignSystem"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/darshilv/AtmakoshiOSDesignSystem.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
