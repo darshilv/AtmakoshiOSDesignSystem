4dadfbb8abc2e73604a851ff04fd803b49f89c14
Ashna Kothari <ashnakothari@gmail.com>
2026-02-16 17:19:37 -0800

    fix: removing its own dependency

--- Package.swift
+++ Package.swift
@@ -15,9 +15,7 @@ let package = Package(
             name: "AtmakoshDesignSystem",
             targets: ["AtmakoshDesignSystem"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/darshilv/AtmakoshiOSDesignSystem.git", branch: "main")
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
